1acc022e47a8ee40881fb89240dbcf2e817ece1b
Gniev <qszak@vp.pl>
2022-05-16 10:00:25 +0200

    Added Jest and first tests to project

---
+++ tests/ad-record.test.ts
@@ -0,0 +1,32 @@
+import { AdRecord } from "../records/ad.record"
+
+const defaultObj = {
+    id: "11",
+    name: "testName",
+    description: "blah",
+    url: "https://megak.pl",
+    price: 0,
+    lat: 9,
+    lon: 9,
+}
+
+
+test("Can build AdRecord", () =>{
+    const ad = new AdRecord(defaultObj);
+
+    expect(ad.name).toBe("testName")
+    expect(ad.description).toBe("blah")
+});
+
+test('Validates invalid price', ()=>{
+    expect(()=> new AdRecord({
+        ...defaultObj,
+        price: -2
+
+})).toThrow('Cena nie może być mniejsza niż zero  ani przekraczać 9999999')
+
+
+})
+
+
+//TODO 
